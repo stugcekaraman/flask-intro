6f7f6e1d828d46f935a51e8ed112d3cb363acc91
stugcekaraman <tugcekaraman3@gmail.com>
2023-03-17 19:05:48 +0300

    mysql cursor bug fix

--- blog.py
+++ blog.py
@@ -1,40 +1,65 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from flaskext.mysql import MySQL
+import mysql.connector
+from wtforms import Form, StringField, TextAreaField, PasswordField, EmailField,validators
 from passlib.hash import sha256_crypt
+# from mysqlx import connection
 
 # Kullanıcı Kayıt Formu
 
 
 class RegisterForm(Form):
     name = StringField("İsim Soyisim", validators=[
-                       validators.length(min=4, max=25)])
+                       validators.Length(min=4, max=25)])
     username = StringField("Kullanıcı Adı", validators=[
-                           validators.length(min=5, max=35)])
-    email = StringField(
-        "E-mail Adresi", validators=[validators.Email(message="Geçerli E-Mail Giriniz.")])
+                           validators.Length(min=5, max=35)])
+    email = EmailField(
+        "E-mail Adresi", validators=[validators.Email(message="Geçerli bir e-mail giriniz.")])
     password = PasswordField("Parola:", validators=[validators.DataRequired(
         message="Lütfen bir parola belirleyin."), validators.EqualTo(fieldname="confirm", message="Parolanız Uyuşmuyor")])
     confirm = PasswordField("Parola Doğrula")
 
 
-app = Flask(__name__)
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "ybblog"
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
-mysql = MySQL(app)
+
+
+app = Flask(__name__)
+mydb = mysql.connector.connect(host = "localhost", user="root",password="",database="ybblog",port=3306)
+
+app.secret_key="ybblog"
+# app.config["MYSQL_HOST"] = "localhost"
+# app.config["MYSQL_USER"] = "root"
+# app.config["MYSQL_PASSWORD"] = ""
+# app.config["MYSQL_DB"] = "ybblog"
+# app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+# mysqld = MySQL(app)
+# mydb = mysql.connector.connect()
+
+
+# mysql.init_app(app)
+
+
+
+def runSql(id, *args):
+    cursor = mydb.cursor()
+    if id == 'register':
+        form = args[0]
+        sorgu = "Insert into user_table(name, email, username, password) VALUES(%s,%s,%s,%s)"
+        cursor.execute(
+            sorgu,
+            (
+            form['name'],
+            form['email'],
+            form['username'],
+            form['password']
+            )
+        )
+        mydb.commit()
+    cursor.close()
 
 
 @app.route('/')
 def index():
-    # articles = [
-    #     {"id": 1, "title": "Deneme1", "content": "Deneme1 İçerik"},
-    #     {"id": 2, "title": "Deneme2", "content": "Deneme2 İçerik"},
-    #     {"id": 3, "title": "Deneme3", "content": "Deneme3 İçerik"}
-    # ]
     return render_template("index.html")
 
 
@@ -44,10 +69,36 @@ def about():
 
 # Kayıt Olma
 
-
-@app.route("/register")
+@app.route("/register", methods = ["GET","POST"])
 def register():
-    return render_template("register.html")
+    form = RegisterForm(request.form)
+    if request.method =="POST" and form.validate():
+        #FORM ITEMS
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.hash(form.password.data)
+        
+        # SQL FUNCTION
+        runSql('register', {
+            "name": name,
+            "username": username,
+            "email": email,
+            "password": password
+        })
+
+        # cursor = mydb.cursor()
+        # sorgu = "Insert into user_table(name, email, username, password) VALUES(%s,%s,%s,%s)"
+        # cursor.execute(sorgu,(name,email,username,password))
+        # mydb.commit()
+        # cursor.close()
+        flash("Başarıyla Kayıt Oldunuz...","success")
+
+        # return redirect(url_for("index"))
+
+        return render_template("register.html",form=form)
+    else:
+        return render_template("register.html",form=form)
 
 
 @app.route("/article/<string:id>")
