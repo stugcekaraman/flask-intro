e0b3c8e37ddd56058d50e81cfafdd7a4de483ef1
stugcekaraman <tugcekaraman3@gmail.com>
2023-03-22 23:42:33 +0300

    message view converted to alert

--- blog.py
+++ blog.py
@@ -18,6 +18,10 @@ class RegisterForm(Form):
     password = PasswordField("Parola:", validators=[validators.DataRequired(
         message="Lütfen bir parola belirleyin."), validators.EqualTo(fieldname="confirm", message="Parolanız Uyuşmuyor")])
     confirm = PasswordField("Parola Doğrula")
+class LoginForm(Form):
+    
+    username = StringField("Kullanıcı Adı")
+    password = PasswordField("Parola")
 
 
 
@@ -27,16 +31,7 @@ app = Flask(__name__)
 mydb = mysql.connector.connect(host = "localhost", user="root",password="",database="ybblog",port=3306)
 
 app.secret_key="ybblog"
-# app.config["MYSQL_HOST"] = "localhost"
-# app.config["MYSQL_USER"] = "root"
-# app.config["MYSQL_PASSWORD"] = ""
-# app.config["MYSQL_DB"] = "ybblog"
-# app.config["MYSQL_CURSORCLASS"] = "DictCursor"
-# mysqld = MySQL(app)
-# mydb = mysql.connector.connect()
 
-
-# mysql.init_app(app)
 
 
 
@@ -82,11 +77,30 @@ def register():
         flash("Başarıyla Kayıt Oldunuz...","success")
 
         # Success Redirection
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
     else:
         
         # Unsuccess Redirection
         return render_template("register.html",form=form)
+
+#Login 
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form = LoginForm(request.form)
+    if request.method == "POST":
+        username = form.username.data
+        password_entered = form.password.data
+        cursor=mydb.cursor()
+        sorgu = "Select * From user_table where username = %s"
+        result = cursor.execute(sorgu,(username,))
+        if result >0:
+            pass
+       
+        else:
+            flash("Böyle bir kullanıcı bulunmuyor.","danger")
+            return redirect(url_for("login"))
+
+    return render_template("login.html",form = form)
 
 
 @app.route("/article/<string:id>")
